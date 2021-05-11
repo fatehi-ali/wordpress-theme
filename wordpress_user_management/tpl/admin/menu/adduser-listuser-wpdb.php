084b0035dc950502fcc1082f169be41aa35bb0db
fatehi-ali <79412432+fatehi-ali@users.noreply.github.com>
2021-05-11 10:01:42 -0700

    session 11 -user management in wordpress
    session 11 -user management in wordpress

---
+++ wordpress_user_management/tpl/admin/menu/adduser-listuser-wpdb.php
@@ -0,0 +1,12 @@
+<div class="wrap">
+<h1 style="font-family: IRANSans;">اضافه کردن کاربر جدید</h1>
+<form action="" method="post">
+    <label for="">نام</label>
+    <input name="firstname" type="text">
+    <label for="">نام خانوادگی</label>
+    <input name="lastname"type="text">
+    <label for="">موبایل</label>
+    <input name="mobail" type="text">
+    <input type="submit" name="sendinformationuser" value="ثبت اطلاعات" class="button button-primary">
+</form>
+</div>
