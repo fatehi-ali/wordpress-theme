ab709d7c55f1ad5f9bf40fba04af7a4671e784db
fatehi-ali <79412432+fatehi-ali@users.noreply.github.com>
2021-05-28 23:30:29 -0700

    session 16- simple themes
    session 16- simple themes

---
+++ simple themes/template/front/index/content-index.php
@@ -0,0 +1,40 @@
+<div class="titlebarpub">
+  <div class="col-12">
+    <nav><a href="#">خانه</a> / <a href="#">مقالات</a> / مقالات اپل</nav>
+    <h1>مقالات اپل</h1>
+  </div>
+</div>
+</div>
+<div class="row">
+  <div class="col-12 col-lg-9 ">
+    <div class="row">
+
+      <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post() ?>
+
+          <div class="col-6 col-md-4">
+            <div class="contentbox">
+              <header class="articlebox">
+                <figure>
+                  <a href="#"><img src="images/articlesample.jpg"></a>
+                  <a href="<?php the_permalink(); ?>">
+                    <h2><?php the_title(); ?></h2>
+                  </a>
+                </figure>
+              </header>
+              <div class="datebox"><?php the_time(); ?></div>
+            </div>
+          </div>
+
+        <?php endwhile; ?>
+      <?php endif; ?>
+
+      <div class="pagenumbers">
+        <a href="#">1</a>
+        <a href="#" class="active">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+      </div>
+    </div>
+  </div>
