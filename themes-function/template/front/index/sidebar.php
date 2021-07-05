302ff59c12323a5fb9856d48bcd6bd43d41ee6e0
fatehi-ali <79412432+fatehi-ali@users.noreply.github.com>
2021-07-04 21:29:16 -0700

    session 17- themes-function
    WordPress Theme - Coding in functions.php

---
+++ themes-function/template/front/index/sidebar.php
@@ -0,0 +1,64 @@
+<div class="d-none d-sm-block col-12 col-lg-3">
+  <aside>
+    <div class="sidebox">
+      <header>
+        <h4>آخرین مقالات</h4>
+      </header>
+      <section>
+        <ul class="postsideseri">
+          <li>
+            <a href="#">
+              <img src="images/sidearticle.jpg" alt="">
+            </a>
+            <a href="#">
+              <h3>آی او اس ۱۱ چه امکاناتی فوق العاده ای دارد؟</h3>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="images/sidearticle.jpg" alt="">
+            </a>
+            <a href="#">
+              <h3>آی او اس ۱۱ چه امکاناتی فوق العاده ای دارد؟</h3>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="images/sidearticle.jpg" alt="">
+            </a>
+            <a href="#">
+              <h3>آی او اس ۱۱ چه امکاناتی فوق العاده ای دارد؟</h3>
+            </a>
+          </li>
+
+        </ul>
+        <a href="#" class="cta">مشاهده همه مقالات</a>
+      </section>
+    </div>
+    <div class="sidebox">
+      <header>
+        <h4>بنرهای پیشنهادی</h4>
+      </header>
+      <section>
+        <img src="images/logo3.jpg" alt="">
+        <img src="images/logo2.jpg" alt="">
+        <img src="images/logo1.jpg" alt="">
+      </section>
+    </div>
+    <div class="sidebox">
+      <header>
+        <h4>ابزارک عمومی</h4>
+      </header>
+      <section>
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود
+        </p>
+        <ul>
+          <li><a href="#">گزینه اول</a></li>
+          <li><a href="#">گزینه دوم</a></li>
+          <li><a href="#">گزینه سوم</a></li>
+        </ul>
+      </section>
+    </div>
+  </aside>
+</div>
